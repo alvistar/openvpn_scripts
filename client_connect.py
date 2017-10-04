1997ca3a74b6987936c72d93208474ffcbf2b804
Alessandro Viganò <alvistar@gmail.com>
2017-10-05 00:56:58 +0200

    Fixed paths

--- client_connect.py
+++ client_connect.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 
-import os, warnings, sys
+import os
+import warnings
+import sys
 
 from textwrap import dedent
 import ruamel
@@ -9,9 +11,9 @@ from ruamel.yaml import YAML
 from pykwalify.core import Core
 
 class Leases():
-  FOLDER = '/usr/local/openvpn_scripts/'
-  LEASES_FILE = FOLDER + 'leases.yaml'
-  SCHEMA = FOLDER + 'schema.yaml'
+  FOLDER = sys.path[0]
+  LEASES_FILE = os.path.join(FOLDER, 'leases.yaml')
+  SCHEMA = os.path.join(FOLDER, 'schema.yaml')
 
   def __init__(self):
     self.config = None
@@ -53,7 +55,7 @@ class Leases():
     try:
       address = self.config['clients'].get(client)
     except KeyError:
-      self.config['clients']={}
+      self.config['clients'] = {}
 
     if address is None:
       address = "{0}{1:02x}".format(self.config['prefix'], self.config['next'])
@@ -64,19 +66,19 @@ class Leases():
   def save_file(self, address, filename):
     output = """\
       iroute-ipv6 {0}::/64
-      push "setenv-safe RADVD {0}::/64"    
+      push "setenv-safe RADVD {0}::/64"
     """.format(address)
 
     stream = open(filename, 'w')
     stream.write(dedent(output))
     stream.close()
 
-print ('Invoked client_connect with {}'.format(str(sys.argv)))
+print('Invoked client_connect with {}'.format(str(sys.argv)))
 
-CLIENT = os.environ.get ('common_name')
+CLIENT = os.environ.get('common_name')
 FILE = sys.argv[1]
 
-print ('Client common name {}'.format(CLIENT))
+print('Client common name {}'.format(CLIENT))
 
 L = Leases()
 ADDRESS = L.get_address(CLIENT)
